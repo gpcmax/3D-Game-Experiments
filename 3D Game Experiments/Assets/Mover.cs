e4173986100f7158603e71a774c89558e8d3e2a6
Gage <gage.copelin@gmail.com>
2019-01-29 22:09:16 -0500

    added some character movement

---
+++ 3D Game Experiments/Assets/Mover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+
+    private Vector3 goal;
+    private NavMeshAgent agent;
+    public GameObject CameraParent;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+
+        goal = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        goal = transform.position + CameraParent.transform.right * Input.GetAxis("Horizontal") + CameraParent.transform.forward * Input.GetAxis("Vertical");
+        agent.destination = goal;
+	}
+}
